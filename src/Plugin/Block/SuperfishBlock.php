3d52697c991b9df8b9f3168abce56e005af5c495
mehrpadin <mail@mehrpadin.net>
2018-05-20 23:25:56 +0100

    Issue #2951400 by MRPRAVIN: Found coding standard issues in superfish

--- src/Plugin/Block/SuperfishBlock.php
+++ src/Plugin/Block/SuperfishBlock.php
@@ -52,7 +52,7 @@ class SuperfishBlock extends SystemMenuBlock {
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, array $plugin_definition) {
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     return new static(
       $configuration,
       $plugin_id,
